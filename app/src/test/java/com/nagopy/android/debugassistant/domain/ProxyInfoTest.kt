3a8dcdf807a0e19d076b8138a245d07196e71972
75py <dev.75py@gmail.com>
2022-03-26 15:01:51 +0900

    add a test case

---
+++ app/src/test/java/com/nagopy/android/debugassistant/domain/ProxyInfoTest.kt
@@ -0,0 +1,16 @@
+package com.nagopy.android.debugassistant.domain
+
+import org.junit.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class ProxyInfoTest {
+
+    @Test
+    fun isAvailable() {
+        assertFalse(ProxyInfo("", "").isAvailable())
+        assertFalse(ProxyInfo("localhost", "").isAvailable())
+        assertFalse(ProxyInfo("", "8888").isAvailable())
+        assertTrue(ProxyInfo("localhost", "8888").isAvailable())
+    }
+}
