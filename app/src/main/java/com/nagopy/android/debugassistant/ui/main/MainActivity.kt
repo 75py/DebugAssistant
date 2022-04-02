90216bb669530dca86099edeccdb9db055874e63
75py <dev.75py@gmail.com>
2022-04-02 17:04:17 +0900

    fix: enable to scroll vertically

--- app/src/main/java/com/nagopy/android/debugassistant/ui/main/MainActivity.kt
+++ app/src/main/java/com/nagopy/android/debugassistant/ui/main/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Divider
@@ -124,7 +126,11 @@ fun MainScreen(
     onHowToUseButtonClicked: () -> Unit,
     onLicensesButtonClicked: () -> Unit,
 ) {
-    Column(Modifier.padding(16.dp)) {
+    Column(
+        Modifier
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
         if (!isPermissionGranted) {
             PermissionErrorMessage(onAdbCommandClicked = onAdbCommandClicked)
 
